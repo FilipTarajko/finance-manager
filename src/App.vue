29ff61f8feb47812cf398a06ba9d34434fddb820
ft <filiptarajko@gmail.com>
2024-02-11 12:00:09 +0100

    Add a simple export system

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/statistics">Statistics</RouterLink>
+    <RouterLink to="/export">Export</RouterLink>
   </nav>
 
   <RouterView />
--- src/router/index.ts
+++ src/router/index.ts
@@ -12,10 +12,12 @@ const router = createRouter({
     {
       path: '/statistics',
       name: 'statistics',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/StatisticsView.vue')
+    },
+    {
+      path: '/export',
+      name: 'export',
+      component: () => import('../views/ExportView.vue')
     }
   ]
 })
