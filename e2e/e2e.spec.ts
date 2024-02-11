81c687870fdbded148df108e83b99392fb3d2516
ft <filiptarajko@gmail.com>
2024-02-11 21:11:18 +0100

    Add category creation to tests

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('E2E test', async ({ page }) => {
   await page.goto('/');
+
+  // Transactions - create
   await expect(page.getByRole('heading', { name: "Transactions" })).toBeVisible();
   await page.getByRole('textbox').click();
   await page.getByRole('textbox').fill('qwe');
@@ -10,4 +12,13 @@ test('E2E test', async ({ page }) => {
   await page.getByRole('combobox').selectOption('[object Object]');
   await page.getByRole('button', { name: 'add' }).click();
   await expect(page.getByText('qwe: 345.57')).toBeVisible();
+
+  // Category - create
+  await page.getByRole('link', { name: 'Categories' }).click();
+  await page.locator('input[type="text"]').click();
+  await page.locator('input[type="text"]').fill('qwerrtty');
+  await page.locator('input[type="color"]').click();
+  await page.locator('input[type="color"]').fill('#aa0909');
+  await page.getByRole('button', { name: 'add' }).click();
+  await expect(page.getByText('qwerrtty')).toBeVisible();
 });
---
+++ src/stores/__tests__/categoriesStore.spec.ts
@@ -0,0 +1,18 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { useCategoriesStore } from '../categoriesStore'
+import { describe, beforeEach, it, expect } from 'vitest'
+
+describe('categoriesStore', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+
+    it('adds new category', () => {
+        const categoriesStore = useCategoriesStore()
+        categoriesStore.addCategory('test category name', "#afb034", "testIconName")
+        let lastElement = categoriesStore.categories[categoriesStore.categories.length - 1]
+        expect(lastElement.name).toEqual('test category name')
+        expect(lastElement.color).toEqual("#afb034")
+        expect(lastElement.icon).toEqual('testIconName')
+    })
+})
