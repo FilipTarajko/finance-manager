255339ee07b677215f6a9e3d928495b485591077
ft <filiptarajko@gmail.com>
2024-02-12 20:50:21 +0100

    Adjust E2E test to match new form UI

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -5,19 +5,21 @@ test('E2E test', async ({ page }) => {
 
   // Transactions - create
   await expect(page.getByRole('heading', { name: "Transactions" })).toBeVisible();
-  await page.getByRole('textbox').click();
-  await page.getByRole('textbox').fill('qwe');
-  await page.getByRole('spinbutton').click();
-  await page.getByRole('spinbutton').fill('345.57');
-  await page.getByRole('combobox').selectOption('[object Object]');
-  await page.getByRole('button', { name: 'add' }).click();
-  await expect(page.getByText('qwe: 345.57')).toBeVisible();
+  await page.getByLabel('Name').click();
+  await page.getByLabel('Name').fill('testname');
+  await page.getByLabel('Amount').click();
+  await page.getByLabel('Amount').fill('345.67');
+  await page.getByRole('combobox').locator('div').filter({ hasText: 'ItemItem' }).locator('div').click();
+  await page.getByRole('option', { name: 'Food' }).click();
+  await page.getByRole('button', { name: 'submit' }).click();
+  await expect(page.getByText('testname: 345.67')).toBeVisible();
 
   // Category - create
   await page.getByRole('link', { name: 'Categories' }).click();
-  await page.locator('input[type="text"]').click();
-  await page.locator('input[type="text"]').fill('qwerrtty');
-  await page.locator('input[type="color"]').fill('#aa0909');
-  await page.getByRole('button', { name: 'add' }).click();
-  await expect(page.getByText('qwerrtty')).toBeVisible();
+  await page.getByLabel('Name').click();
+  await page.getByLabel('Name').fill('qwertyuiop');
+  await page.locator('.v-slider-track__background').click();
+  await page.locator('button:nth-child(7)').first().click();
+  await page.getByRole('button', { name: 'submit' }).click();
+  await expect(page.getByText('qwertyuiop')).toBeVisible();
 });
