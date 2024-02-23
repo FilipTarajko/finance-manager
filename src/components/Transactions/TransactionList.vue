09c803988a0b68b8a4d8b417698f8c8cae2c4c57
ft <filiptarajko@gmail.com>
2024-02-23 22:22:40 +0100

    Make TransactionList height depend on transactions amount

--- src/components/Transactions/TransactionList.vue
+++ src/components/Transactions/TransactionList.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
 import TransactionComponent from '@/components/Transactions/TransactionComponent.vue'
 import type { TransactionWithCategoryData } from '@/types/types';
+import { computed } from 'vue';
 
-defineProps<{
+const height_limit = 300
+const height_per_transaction = 25.5
+const height_after_last_transaction = 15
+
+const props = defineProps<{
   showDialog: Function,
   transactions: TransactionWithCategoryData[]
 }>()
 
+const height_for_all_transactions = computed(() => {
+  return props.transactions.length * height_per_transaction + height_after_last_transaction;
+})
 
+const height = computed(() => {
+  if (height_for_all_transactions.value < height_limit) {
+    return height_for_all_transactions.value;
+  } else {
+    return height_limit;
+  }
+})
 </script>
 
 <template>
@@ -15,7 +30,7 @@ defineProps<{
 
     <v-card width="500">
       <v-virtual-scroll
-        :height="300"
+        :height="height"
         :items="transactions"
       >
         <template v-slot:default="{ item }">
