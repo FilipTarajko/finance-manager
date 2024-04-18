c7db8a0ecbfe52fd8f8653ece124647afd4c912d
ft <filiptarajko@gmail.com>
2024-04-18 17:59:10 +0200

    Add alias for categories route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'categories',
+      alias: '/categories',
       component: () => import('../views/CategoriesView.vue')
     },
     {
