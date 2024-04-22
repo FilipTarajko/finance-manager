9959202583e3ccae4adcb8bafa951b4c000db0aa
ft <filiptarajko@gmail.com>
2024-04-22 19:28:52 +0200

    Allow tabbing over navigation buttons

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,34 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useThemeStore } from "@/stores/themeStore"
 import { useSnackbarStore } from './stores/snackbarStore';
-import { ref, type Ref } from 'vue';
+import { computed, ref, type Ref } from 'vue';
+import { onUnmounted } from 'vue';
 
+const BREAKPOINT_WIDTH = 900;
 const themeStore = useThemeStore()
 const snackbarStore = useSnackbarStore()
-
 const drawer: Ref<any> = ref(null);
+
+const topBarLinkTabIndex = computed(() => {
+  return width.value > BREAKPOINT_WIDTH ? 0 : -1;
+});
+
+const drawerLinkTabIndex = computed(() => {
+  return drawer.value ? 0 : -1;
+});
+
+let width = ref(window.innerWidth);
+
+function updateWindowWidth() {
+  console.log('updateWindowWidth')
+  width.value = window.innerWidth;
+}
+
+window.addEventListener('resize', updateWindowWidth);
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWindowWidth);
+});
 </script>
 
 <template>
@@ -21,19 +43,19 @@ const drawer: Ref<any> = ref(null);
         </v-btn>
         <div>
           <v-toolbar-title style="font-size: 1.4rem;">
-            <RouterLink id="logo" to="/" >
+            <RouterLink id="logo" to="/" tabindex="0" >
               Finance Manager
             </RouterLink>
           </v-toolbar-title>
         </div>
         <nav class="display-if-not-narrow-screen"
           style="flex-direction: row; justify-content: space-between; align-items: baseline;">
-          <RouterLink class="layoutRouterLink layoutRouterLink--left" :to="{ name: 'categories' }">Categories</RouterLink>
-          <RouterLink class="layoutRouterLink" :to="{ name: 'transactions' }">Transactions</RouterLink>
-          <RouterLink class="layoutRouterLink" :to="{ name: 'accounts' }">Accounts</RouterLink>
-          <RouterLink class="layoutRouterLink" :to="{ name: 'currencies' }">Currencies</RouterLink>
-          <RouterLink class="layoutRouterLink" :to="{ name: 'statistics' }">Statistics</RouterLink>
-          <RouterLink class="layoutRouterLink" :to="{ name: 'importexport' }">Import/Export</RouterLink>
+          <RouterLink  class="layoutRouterLink layoutRouterLink--left" :to="{ name: 'categories' }" :tabindex="topBarLinkTabIndex">Categories</RouterLink>
+          <RouterLink class="layoutRouterLink" :to="{ name: 'transactions' }" :tabindex="topBarLinkTabIndex">Transactions</RouterLink>
+          <RouterLink class="layoutRouterLink" :to="{ name: 'accounts' }" :tabindex="topBarLinkTabIndex">Accounts</RouterLink>
+          <RouterLink class="layoutRouterLink" :to="{ name: 'currencies' }" :tabindex="topBarLinkTabIndex">Currencies</RouterLink>
+          <RouterLink class="layoutRouterLink" :to="{ name: 'statistics' }" :tabindex="topBarLinkTabIndex">Statistics</RouterLink>
+          <RouterLink class="layoutRouterLink" :to="{ name: 'importexport' }" :tabindex="topBarLinkTabIndex">Import/Export</RouterLink>
         </nav>
         <v-btn variant="outlined" style="min-width: 0px; width: 50px;" @click="themeStore.toggleTheme">
           <v-icon icon="mdi-theme-light-dark"></v-icon>
@@ -43,31 +65,31 @@ const drawer: Ref<any> = ref(null);
 
     <v-navigation-drawer id="drawer" v-model="drawer" temporary>
       <v-list density="compact" nav style="display: flex; flex-direction: column;">
-        <v-btn style="margin: 0.5rem;" color="primary" @click.stop="drawer = false">
+        <v-btn style="margin: 0.5rem;" color="primary" @click.stop="drawer = false" :tabindex="drawerLinkTabIndex">
           Close
         </v-btn>
         <!-- <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item> -->
-        <RouterLink class="layoutRouterLink" :to="{ name: 'categories' }">
+        <RouterLink class="layoutRouterLink" :to="{ name: 'categories' }" :tabindex="drawerLinkTabIndex">
           <v-icon color="primary" icon="mdi-shape"></v-icon>
           Categories
         </RouterLink>
-        <RouterLink class="layoutRouterLink" :to="{ name: 'transactions' }">
+        <RouterLink class="layoutRouterLink" :to="{ name: 'transactions' }" :tabindex="drawerLinkTabIndex">
           <v-icon icon="mdi-dots-grid"></v-icon>
           Transactions
         </RouterLink>
-        <RouterLink class="layoutRouterLink" :to="{ name: 'accounts' }">
+        <RouterLink class="layoutRouterLink" :to="{ name: 'accounts' }" :tabindex="drawerLinkTabIndex">
           <v-icon color="warning" icon="mdi-credit-card"></v-icon>
           Accounts
         </RouterLink>
-        <RouterLink class="layoutRouterLink" :to="{ name: 'currencies' }">
+        <RouterLink class="layoutRouterLink" :to="{ name: 'currencies' }" :tabindex="drawerLinkTabIndex">
           <v-icon color="var(--visible-yellow)" icon="mdi-currency-usd"></v-icon>
           Currencies
         </RouterLink>
-        <RouterLink class="layoutRouterLink" :to="{ name: 'statistics' }">
+        <RouterLink class="layoutRouterLink" :to="{ name: 'statistics' }" :tabindex="drawerLinkTabIndex">
           <v-icon color="green" icon="mdi-chart-bar"></v-icon>
           Statistics
         </RouterLink>
-        <RouterLink class="layoutRouterLink" :to="{ name: 'importexport' }">
+        <RouterLink class="layoutRouterLink" :to="{ name: 'importexport' }" :tabindex="drawerLinkTabIndex">
           <v-icon color="cyan" icon="mdi-export"></v-icon>
           Import/Export
         </RouterLink>
